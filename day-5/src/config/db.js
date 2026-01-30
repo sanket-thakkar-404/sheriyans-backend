b6789cab91307924bfeec4f8380ea8f1f5b4a8f3
sanket-thakkar-404 <sanketthakkar500@gmail.com>
2026-01-30 14:53:31 +0530

    day 5 compeleted

--- day-5/src/config/db.js
+++ day-5/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://sanket_thakkar:10mmIMvNqUwnENjk@cluster0.ujlxv8u.mongodb.net/day-5');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
